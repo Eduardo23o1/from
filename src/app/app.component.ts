e6f0fe41aad2a8eb3bd46011a4d6b5be4e0f9d25
Eduardo23o1 <ejosetapia@unicesar.edu.co>
2023-10-18 10:00:06 -0500

    primer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'from';
+  selectedFile: File | null = null;
+  datos: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.consultar(); 
+  }
+  
+  onFileChanged(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  consultar() {
+    this.http.get("http://localhost:4000/mi-api-rest/consultarArchivo").subscribe((data) => {this.datos = data as any[]; console.log(this.datos);}, (error => {console.log(error);}));
+  }
+
+  onFileSelected() {
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('nameArchivo', this.selectedFile, this.selectedFile.name);
+
+      this.http.post('http://localhost:4000/mi-api-rest/RegistrarArchivo', formData)
+        .subscribe(
+          (response) => {
+            console.log('Archivo subido exitosamente:', response);
+          },
+          (error) => {
+            console.log('Error al subir el archivo:', error);
+          }
+        );
+    } else {
+      console.log('Ningún archivo seleccionado.');
+    }
+  }
 }
